58f23018bd33a92a7fbab1ed1d80fc24e2b9cc25
Felipe Mendes Neves <felipemen74@gmail.com>
2025-07-26 17:33:13 -0300

    feat: adiciona funcionalidade de deletar filmes

--- src/main/java/com/filmsreviews/filmsreviews_api/controller/FilmController.java
+++ src/main/java/com/filmsreviews/filmsreviews_api/controller/FilmController.java
@@ -2,15 +2,20 @@ package com.filmsreviews.filmsreviews_api.controller;
 
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.filmsreviews.filmsreviews_api.controller.dto.CreateFilmDto;
 import com.filmsreviews.filmsreviews_api.entities.Film;
+import com.filmsreviews.filmsreviews_api.entities.Role;
 import com.filmsreviews.filmsreviews_api.repository.FilmRepository;
 import com.filmsreviews.filmsreviews_api.repository.UserRepository;
 
@@ -37,6 +42,26 @@ public class FilmController {
         film.setDescription(dto.description());
         film.setImage(dto.image());
         filmRepository.save(film);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/films/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    public ResponseEntity<Void> deleteFilm(@PathVariable("id") Long id, JwtAuthenticationToken token) {
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+
+        var film = filmRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        
+        var isAdmin = user.get().getRoles()
+            .stream()
+            .anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+        
+        if(isAdmin || film.getUser().getId().equals(UUID.fromString(token.getName()))) {
+            filmRepository.deleteById(id);
+        } else {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+        
         return ResponseEntity.ok().build();
     }
 }
